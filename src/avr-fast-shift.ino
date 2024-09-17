ad986ade58153570c00b90edda812c663f8a2e10
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-09-17 08:53:10 -0500

    The example code is the default sketch.

--- src/avr-fast-shift.ino
+++ src/avr-fast-shift.ino
@@ -1,20 +1,4 @@
-#include <Arduino.h>
-#include "avr-fast-shift.h"
-
-void setup()
-{
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    // NOTE!!! Wait for >2 secs
-    // if board doesn't support software reset via Serial.DTR/RTS
-    delay(2000);
-}
-
-void loop()
-{
-    // Blink to indicate end of test
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(250);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(250);
-}
+/*
+ avr-fast-div example
+*/
+#include "../examples/example/example.ino"
